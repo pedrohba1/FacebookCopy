79a3da999ab1d38021e74aa51a483d15f9814eb0
Pedro Bufulin <pedro.almeida@ufu.br>
2020-02-15 22:51:20 -0300

    demonstração completa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,36 @@
 import React, { Component } from "react";
+import MaterialIcon from "material-icons-react";
+import "../styles/header.css";
 
 class Header extends Component {
   state = {};
   render() {
     return (
-      <nav className="navbar navbar-light" style={styles.header}>
-        <span className="navbar-brand mb-0 h1" style={styles.font}>
-          Facebook
-        </span>
+      <nav className="navbar header-color">
+        <ul className="nav navbar-right inline">
+          <li>
+            <span className="font-big">facebook</span>
+            <MaterialIcon
+              color={"#fff"}
+              size={8}
+              icon="copyright"
+            ></MaterialIcon>
+          </li>
+        </ul>
+
+        <a>
+          <ul className="nav navbar-right inline">
+            <li>
+              <span className="font-profile">Meu perfil </span>
+            </li>
+            <li className="ml-2">
+              <MaterialIcon color={"#fff"} icon="person_pin"></MaterialIcon>
+            </li>
+          </ul>
+        </a>
       </nav>
     );
   }
 }
 
 export default Header;
-
-const styles = {
-  header: {
-    backgroundColor: "#4A90E2"
-  },
-  font: {
-    color: "#fff"
-  }
-};
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Post from "./Post";
 
 class PostList extends Component {
   state = {
@@ -7,10 +8,10 @@ class PostList extends Component {
         id: 1,
         author: {
           name: "Julio Alcantara",
-          avatar: "http://url-da-imagem.com/imagem.jpg"
+          avatar: "https://imgur.com/gallery/6z2f1c7"
         },
         date: "04 Jun 2019",
-        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
         comments: [
           {
             id: 1,
@@ -18,14 +19,45 @@ class PostList extends Component {
               name: "Diego Fernandes",
               avatar: "http://url-da-imagem.com/imagem.jpg"
             },
-            content: "Conteúdo do comentário"
+            content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`
           }
         ]
       },
       {
-        id: 2
-        // Restante dos dados de um novo post
+        id: 1,
+        author: {
+          name: "Julio Alcantara",
+          avatar: "https://imgur.com/gallery/6z2f1c7"
+        },
+        date: "04 Jun 2019",
+        content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Diego Fernandes",
+              avatar: "http://url-da-imagem.com/imagem.jpg"
+            },
+            content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`
+          }
+        ]
       }
     ]
   };
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.posts.map(post => (
+          <div className="container mt-5">
+            <Post key={post.id} post={post}></Post>
+          </div>
+        ))}
+      </React.Fragment>
+    );
+  }
 }
+
+export default PostList;
